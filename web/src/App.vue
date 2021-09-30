b9186e05ef17a800e32ff620e14aa868ca235a47
刘有泽 <liuyouze@cmict.chinamobile.com>
2021-09-30 10:25:51 +0800

    添加vue router，添加导航栏

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavMenuHor/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import NavMenuHor from '@/components/NavMenuHor.vue'
+
+export default {
+  components: {
+    NavMenuHor
+  }
+}
+</script>
+
 
 <style>
 #app {

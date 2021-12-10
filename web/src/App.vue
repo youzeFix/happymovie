ff137e5258d62b36f3fef554bf1ced309a858752
刘有泽 <liuyouze@cmict.chinamobile.com>
2021-12-10 14:42:25 +0800

    增加备案信息

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,6 +4,10 @@
     <keep-alive include="movie-library,movie-pick">
       <router-view/>
     </keep-alive>
+    <el-divider></el-divider>
+    <div style="width:300px;margin:0 auto; padding:20px 0;">
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=13010202002976" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="./assets/备案图标.png" style="float:left;" alt="备案图标"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">冀公网安备 13010202002976号</p></a>
+    </div>
   </div>
 </template>
 

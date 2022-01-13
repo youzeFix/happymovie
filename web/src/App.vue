26c678a2b858f0bdcaac2a1269db56f98afb004e
刘有泽 <liuyouze@cmict.chinamobile.com>
2022-01-13 14:22:05 +0800

    Update App.vue
    添加备案号

--- web/src/App.vue
+++ web/src/App.vue
@@ -5,8 +5,23 @@
       <router-view/>
     </keep-alive>
     <el-divider></el-divider>
-    <div style="width:300px;margin:0 auto; padding:20px 0;">
-        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=13010202002976" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="./assets/备案图标.png" style="float:left;" alt="备案图标"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">冀公网安备 13010202002976号</p></a>
+    <div style="width:600px;margin:0 auto; padding:20px 0;text-align:center">
+      <el-row >
+        <el-col :span=14>
+          <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=13010202002976" 
+          style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="./assets/备案图标.png" 
+          style="float:left;" alt="备案图标"/>
+          <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">冀公网安备 13010202002976号</p></a>
+        </el-col>
+
+        <el-col :span=10>
+          <a target="_blank" href="https://beian.miit.gov.cn/" 
+          style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+          <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">冀ICP备2021026594号-1</p></a>
+        </el-col>
+      </el-row>
+        
+        
     </div>
   </div>
 </template>

ae44074bf1e140a333fe8a826c0977a18df66926
UsielLau <824797605@qq.com>
2022-06-12 21:03:45 +0800

    Update MovieLibrary.vue
    自动解析增加下拉菜单

--- web/src/components/MovieLibrary.vue
+++ web/src/components/MovieLibrary.vue
@@ -3,9 +3,17 @@
     <div class="button-group">
         <el-button type="success" icon="el-icon-refresh" @click="loadMovieData()">刷新</el-button>
         <el-button-group class="button-group-sub">
-        <el-button type="primary" icon="el-icon-edit-outline" @click="editMovieData()" :disabled='edit_disabled'>编辑</el-button>
-        <el-button type="primary" icon="el-icon-plus" @click="addMovieData()">新增</el-button>
-        <el-button type="danger" icon="el-icon-delete" @click="deleteMovieData()">删除</el-button>
+          <el-button type="primary" icon="el-icon-edit-outline" @click="editMovieData()" :disabled='edit_disabled'>编辑</el-button>
+          
+          <el-dropdown>
+            <el-button type="primary" icon="el-icon-plus" @click="addMovieData()">新增</el-button>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item>自动解析</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          
+          <el-button type="danger" icon="el-icon-delete" @click="deleteMovieData()">删除</el-button>
         </el-button-group>
         <el-button-group class="button-group-sub">
         <el-button type="success" icon="el-icon-upload2" @click="bulkImport()">批量导入</el-button>
